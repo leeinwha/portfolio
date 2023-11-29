576235b3b740b824c08aa6f9ba08f2f9c55184ab
TrendMecca-UIUX <aljard@naver.com>
2023-11-29 16:01:09 +0900

    trendmecca 2023/11/29 Hamilton

--- trendmecca/hamilton/script/script.js
+++ trendmecca/hamilton/script/script.js
@@ -3,18 +3,78 @@ $(document).ready(function() {
         scrollTrigger:{
             trigger:".section2",
             pin: true,
-            start:'top 30%',
-            end:'100% -10%',
+            start:'top 50%',
+            end:'100% -40%',
             scrub:1.5 //start-end
         }
     });
-     tl.to(".sec2_obj1",{y:-250,opacity:1})
-       .to(".sec2_obj2",{y:-250,opacity:1})
-       .to(".sec2_obj3",{y:-250,opacity:1})
+     tl.to(".sec2_obj1",{opacity:1})
+       .to(".sec2_obj1",{opacity:0})
+       .to(".sec2_obj2",{opacity:1})
+       .to(".sec2_obj2",{opacity:0})
+       .to(".sec2_obj3",{opacity:1})
  
- 
-     
-     
+    
+    new Swiper ('.section3_swiper', {
+        loop: true,        
+        speed:800,
+        autoplay: {
+            delay: 1500,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+            1025: {
+                slidesPerView: 3,                
+            },
+            300: {
+                slidesPerView: 1,                
+            },
+        },                
+    });
+
+    new Swiper ('.section4_swiper', {
+        loop: true,        
+        speed:800,
+        slidesPerView: 3,
+        spaceBetween: 20,
+        autoplay: {
+            delay: 1500,
+            disableOnInteraction: false,
+        },                        
+    });
+
+    new Swiper ('.section4_swiper_m', {
+        loop: true,        
+        speed:800,
+        slidesPerView: 2,
+        spaceBetween: 20,
+        autoplay: {
+            delay: 1500,
+            disableOnInteraction: false,
+        },                        
+    });
+
+    new Swiper ('.section5_swiper', {
+        loop: true,        
+        speed:800,
+        slidesPerView: 3,
+        spaceBetween: 20,
+        autoplay: {
+            delay: 1500,
+            disableOnInteraction: false,
+        },                        
+    });
+
+    new Swiper ('.section5_swiper_m', {
+        loop: true,        
+        speed:800,
+        slidesPerView: 2,
+        spaceBetween: 20,
+        autoplay: {
+            delay: 1500,
+            disableOnInteraction: false,
+        },                        
+    });
     
      
  
