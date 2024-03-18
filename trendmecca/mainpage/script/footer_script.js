31c9ce204768152e5f708f9051c8a60ccb6841ee
TrendMecca-UIUX <aljard@naver.com>
2024-03-18 17:16:55 +0900

    2024/03/18 timemecca mainpage (footer)

---
+++ trendmecca/mainpage/script/footer_script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  new Swiper ('.brand_swiper', {
+    loop: true,
+    speed: 1000,
+    autoplay: {
+      delay: 1500,
+      disableOnInteraction: false,
+    },        
+    breakpoints: {
+      1025: {
+        slidesPerView: 4,        
+      },
+      300: {
+        slidesPerView: 4,        
+        autoplay: {
+          delay: 1500,
+          disableOnInteraction: false,
+        },
+      }
+    }    
+  });
+
+  $('.footer_mobile > .footer_list > li').click(function(){
+    $(this).toggleClass('on');
+    $(this).siblings().removeClass('on');
+    $(this).children().next().slideToggle().parent().siblings().find('.depth2').slideUp();
+    $('.arrow_icon').toggleClass('on');
+    $(this).children().next().parent().siblings().find('.arrow_icon').removeClass('on');            
+  });
+
+});
