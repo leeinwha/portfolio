3def642372e2eb2d9039c303b668532857393f30
TrendMecca-UIUX <aljard@naver.com>
2024-03-26 16:16:45 +0900

    2024/03/26 React blog project

--- blog/src/App.js
+++ blog/src/App.js
@@ -9,7 +9,9 @@ function App() {
   let post = '강남 우동 맛집';
   let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동맛집','파이썬독학']);  
   let [따봉, 따봉변경] = useState(0);
-
+  let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
+  let [입력값,입력값변경] = useState('');
 
   return (
     <div className="App">
@@ -23,8 +25,14 @@ function App() {
         글제목변경(copy);
       }}>글수정</button>
 
-      <div className='list'>
-        <h4>{글제목[0]} <span onClick={ () =>{따봉변경(따봉 + 1) }}>👍</span> {따봉} </h4>
+      <button onClick={ ()=>{
+        let copy = [...글제목];
+        copy.sort();
+        글제목변경(copy)
+      } }>정렬버튼</button>
+
+      {/* <div className='list'>
+        <h4>{글제목[0]} <span onClick={ () => { 따봉변경(따봉 + 1) }}>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
@@ -32,11 +40,57 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
-        <h4>{글제목[2]}</h4>
+        <h4 onClick={()=>{ setModal(!modal) }}>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
-      </div>      
+      </div> */}
+
+      {
+        글제목.map(function(a, i){
+          return (
+            <div className='list' key={i}>
+              <h4 onClick={()=>{setModal(true); setTitle(i)}} >
+                { 글제목[i] }
+                <span onClick={(e)=>{ e.stopPropagation(); 따봉변경(따봉+1) }}>👍</span> {따봉}                  
+              </h4>
+              <p>2월 17일 발행</p>
+              <button onClick={()=>{
+                let copy = [...글제목];
+                copy.splice(i, 1)
+                글제목변경(copy);
+              }}>삭제</button>
+            </div>
+          )
+        })
+      }
+
+      <input onChange={(e)=>{ 
+        입력값변경(e.target.value);        
+      }}/>
+      <button onClick={()=>{
+        let copy = [...글제목];
+        copy.unshift(입력값);
+        글제목변경(copy)
+      }}>글발행</button>
+
+      { modal == true ? <Modal title={title} 글제목변경={글제목변경} 글제목={글제목}/> : null }
+
+      
+
     </div>
   );
 }
+
+function Modal(props){
+  return (
+    <div className='modal'>
+      <h4>{props.글제목[props.title]}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+      <button>글수정</button>      
+    </div>
+  )
+};
+
+
 
 export default App;
